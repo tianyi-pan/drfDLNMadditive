aa8fd07aa300d718fc949d22af395df62b85d5f1
tianyi-pan <pantianyi98@163.com>
2026-01-02 10:35:24 -0500

    rate ratio

--- R/RateRatio.R
+++ R/RateRatio.R
@@ -30,9 +30,10 @@ RateRatio.drfDLNMadditive_fit <- function(object, x0, x1, verbose = FALSE, ...) 
   R.CI <- nrow(object$CI.sample[[1]])
   n_alpha_f <- length(object$point$alpha_f)
 
-  cen <- 0
+
 
   out.list <- lapply(1:M, function(i) {
+    cen <- object$data$SS.list[[i]]$SSf$knots[as.integer(kE/2) + 2]
     if (verbose) cat("Exposure", i, "\n")
     ## point estimate
     alpha_f <- object$point$alpha_f[((i-1)*n_alpha_f/M + 1):(i*n_alpha_f/M)]
@@ -68,21 +69,34 @@ RateRatio.drfDLNMadditive_fit <- function(object, x0, x1, verbose = FALSE, ...) 
 
 
     ## CI
-    surface.x0.sample <- sidDLNM:::SurfaceCI(as.matrix(gridEl.x0), object$CI.sample$alpha_f_sample[,((i-1)*n_alpha_f/M + 1):(i*n_alpha_f/M)], cen,
-                                          object$data$SS.list[[i]]$SSf$knots,
-                                          object$data$SS.list[[i]]$Zf, Blag)
-    eta.x0.sample <- colSums(surface.x0.sample)
-
-    surface.x1.sample <- sidDLNM:::SurfaceCI(as.matrix(gridEl.x1), object$CI.sample$alpha_f_sample[,((i-1)*n_alpha_f/M + 1):(i*n_alpha_f/M)], cen,
-                                             object$data$SS.list[[i]]$SSf$knots,
-                                             object$data$SS.list[[i]]$Zf, Blag)
-    eta.x1.sample <- colSums(surface.x1.sample)
-
-
     RR.sample <- lapply(1:R.CI, function(j) {
-      return(data.frame(eta.E.0 = eta.x0.sample[j],
-                        eta.E.1 = eta.x1.sample[j],
-                        RR = exp(eta.x1.sample[j] - eta.x0.sample[j])))
+      if(verbose) {
+        if(j %% 100 == 0) {
+          cat("Sampling iteration:", j, " / ", R.CI, " for exposure",  i, "\n")
+        }
+      }
+      alpha_f <- object$CI.sample$alpha_f_sample[j, ((i-1)*n_alpha_f/M + 1):(i*n_alpha_f/M)]
+
+      eta.E.0 <- sum(apply(gridEl.x0, 1, function(row.) {
+        sidDLNM:::SurfaceEval(row.[1], cen, row.[2], alpha_f,
+                              object$data$SS.list[[i]]$SSf$knots,
+                              object$data$SS.list[[i]]$Zf,
+                              Blag)
+      }))
+
+      eta.E.1 <- sum(apply(gridEl.x1, 1, function(row.) {
+        sidDLNM:::SurfaceEval(row.[1], cen, row.[2], alpha_f,
+                              object$data$SS.list[[i]]$SSf$knots,
+                              object$data$SS.list[[i]]$Zf,
+                              Blag)
+      }))
+
+
+      return(data.frame(eta.E.0 = eta.E.0,
+                           eta.E.1 = eta.E.1,
+                           RR = exp(eta.E.1 - eta.E.0)))
+
+
     })
 
 
